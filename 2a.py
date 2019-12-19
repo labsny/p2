39c0e9db1a522a3b8a56072fd0c6d97408c4d000
labsny <58943251+labsny@users.noreply.github.com>
2019-12-20 03:25:34 +0530

    Add files via upload

---
+++ 2a.py
@@ -0,0 +1,2 @@
+a=input("Enter a string of 4 characters to be converted:")
+print("String in ASCII = ",ord(a[0]),ord(a[1]),ord(a[2]),ord(a[3]))
---
+++ 2b.py
@@ -0,0 +1,4 @@
+n=eval(input("Enter the number to be rounded off: "))
+print("Number in nearest integer = ",round(n,0))
+print("Number in the nearest first decimal = ",round(n,1))
+print("Number in the nearest second decimal = ",round(n,2))
